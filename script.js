b2b3e5468d0c85421f253b6a987531cc9fed0c8b
윤경열 <jjyoon.dev@gmail.com>
2022-12-28 23:44:01 +0900

    React를 이용하여 span 요소를 만들어 html에 삽입하기.

---
+++ script.js
@@ -0,0 +1,3 @@
+const root = document.getElementById("root");
+const span = React.createElement("span", {id: "sexy-span"}, "Hello I'm a span"); // span 엘리먼트 생성
+ReactDOM.render(span, root) // 사용자에게 보여줌
